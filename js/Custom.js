cbf039bff82713121e2512cc6e294b1f08b65ef9
Elkomy <mahmoudelkomy996@gmail.com>
2022-06-13 18:26:10 +0200

    first

---
+++ js/Custom.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+  // MainSlider
+  var swiper = new Swiper('.MainSlider-container', {
+    spaceBetween: 0,
+    centeredSlides: true,
+    loop: true,
+    effect: 'fade',
+    speed: 500,
+    autoplay: {
+      delay: 6000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    keyboard: {
+      enabled: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  // AOS
+  AOS.init();
+  //spinner
+  $(".spinner ").fadeOut("slow");
+  //navbar animation
+  $(window).scroll(function () {
+    var appScroll = $(document).scrollTop();
+    if ((appScroll > 60) && (appScroll < 99999999999)) {
+      $(".mainNav").addClass("animatedNav");
+    };
+    if ((appScroll > 0) && (appScroll < 60)) {
+      $(".mainNav").removeClass("animatedNav");
+    };
+  });
+  // tooltip
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+});
