681c18b15a0b26ee31fc9c2bd668b9b42f57e552
Elkomy <mahmoudelkomy996@gmail.com>
2022-06-15 15:45:50 +0200

    products

--- js/Custom.js
+++ js/Custom.js
@@ -22,38 +22,49 @@ $(document).ready(function () {
       prevEl: '.swiper-button-prev',
     },
   });
-    //newest Slider
-    var swiper = new Swiper('.newestSlider', {
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+  //newest Slider
+  var swiper = new Swiper('.newestSlider', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    // centeredSlides: true,
+    loop: true,
+    // slidesPerView: 'auto',
+    // spaceBetween: 5,
+    speed: 1500,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+
       },
-      // centeredSlides: true,
-      loop: true,
-      // slidesPerView: 'auto',
-      // spaceBetween: 5,
-      speed: 1500,
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
+      360: {
+        slidesPerView: 2,
+
       },
-      breakpoints: {
-        0: {
-          slidesPerView: 1,
-          
-        },
-        360: {
-          slidesPerView: 2,
-          
-        },
-        768: {
-          slidesPerView: 3,
-        },
-        1024: {
-          slidesPerView: 4,
-        },
+      768: {
+        slidesPerView: 3,
       },
-    });
+      1024: {
+        slidesPerView: 4,
+      },
+    },
+  });
+  // filter
+  var $filter = $('.btn').click(function () {
+    if (this.id == 'all') {
+      $('#filterContent > .product').fadeIn(450);
+    } else {
+      var $item = $('.' + this.id).fadeIn(450);
+      $('#filterContent > .product').not($item).hide();
+    }
+    $filter.removeClass('active');
+    $(this).addClass('active');
+  })
   // AOS
   AOS.init({
     offset: 70, // offset (in px) from the original trigger point
