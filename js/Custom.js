a5b8170dec48c4553ee0bbb4e336d6443f90c93c
Elkomy <mahmoudelkomy996@gmail.com>
2022-06-16 17:41:41 +0200

    home

--- js/Custom.js
+++ js/Custom.js
@@ -69,6 +69,23 @@ $(document).ready(function () {
     },
   });
 
+  // validation
+  (function () {
+    'use strict'
+    var forms = document.querySelectorAll('.needs-validation')
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+
+
   // filter
   var $filter = $('.btn').click(function () {
     if (this.id == 'all') {
